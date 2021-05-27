cf378bd97fcda35c5ed81a5ca031c01bea4bbcfb
gimume <yum17201619@gmail.com>
2021-05-27 19:00:58 +0900

    배포 연습

--- CRUD/main/urls.py
+++ CRUD/main/urls.py
@@ -5,13 +5,10 @@ from . import views
 app_name = "main"
 
 urlpatterns=[
-
-    
-    path('/create',views.create,name="create"),
+    path('create',views.create,name="create"),
     path('detail/<int:write_id>',views.detail,name="detail"),
     path('update/<int:write_id>',views.update,name="update"),
     path('delete/<int:write_id>',views.delete, name="delete"),
     path('create_comment/<int:write_id>',views.create_comment,name="create_comment"),
     path('delete_comment/<int:write_id>/<int:comment_id>',views.delete_comment,name="delete_comment"),
-
 ]
